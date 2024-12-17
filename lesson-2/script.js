9140c2d6ae584c7abb298c002e254d911e5e980b
Nick19571101 <nkv682594@gmail.com>
2024-12-17 17:15:34 +0200

    create newfolder 'lesson-2'

---
+++ lesson-2/script.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const phrase = document.getElementById('btn')
+const date = document.getElementById('date')
+const txt = document.querySelector('#txt')
+
+phrase.onclick = function () {
+	txt.innerHTML = 'Hello Nick!'
+}
+phrase.addEventListener('click', () => {
+	date.innerHTML = new Date()
+})
+phrase.addEventListener('click', () => {
+	txt.style.color = 'red'
+})
