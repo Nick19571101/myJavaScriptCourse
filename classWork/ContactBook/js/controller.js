5510f70a669841088dfea725161fa1edee713af8
Nick19571101 <nkv682594@gmail.com>
2025-01-06 08:05:21 +0200

    continuing to work with contactBook

--- classWork/ContactBook/js/controller.js
+++ classWork/ContactBook/js/controller.js
@@ -1,11 +1,15 @@
 const html = document.querySelector('#contact-list')
+const template = document.querySelector('#template').innerHTML
 const contactForm = document.querySelector('#contactForm')
 
+let editId = null
+
 function renderBook() {
-	contactList.render(contactBook.contacts, html)
+	// contactList.render(contactBook.contacts, html)
+	contactList.renderWithTemplate(contactBook.contacts, html, template)
 }
 // contactList.render(contactBook.contacts, html)
-
+// contactList.renderWithTemplate(contactBook.contacts, html, template)
 //inputs
 const firstNameInput = document.querySelector('#firstNameInput')
 const lastNameInput = document.querySelector('#lastNameInput')
@@ -22,6 +26,26 @@ contactForm.addEventListener('submit', e => {
 		email: emailInput.value,
 		description: descriptionInput.value,
 	}
-	contactBook.add(contact)
+	if (editId == null) {
+		contactBook.add(contact)
+	} else {
+		contactBook.update(editId, contact)
+		editId = null
+	}
 	renderBook()
 })
+function remove(id) {
+	contactBook.remove(id)
+	renderBook()
+}
+function edit(id) {
+	const contact = contactBook.find(id)
+	editId = contact.id
+	firstNameInput.value = contact.firstName
+	lastNameInput.value = contact.lastName
+	phoneInput.value = contact.phone
+	emailInput.value = contact.email
+	descriptionInput.value = contact.description
+	renderBook()
+}
+renderBook()
